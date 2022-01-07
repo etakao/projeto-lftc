752e06dee162fbad476ba85c5555f2785fe4e7f5
Erick <ericktakao28@gmail.com>
2022-01-07 18:26:13 -0300

    feat: adicionando automatos finitos

--- src/pages/AF/index.js
+++ src/pages/AF/index.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import Graph from 'react-graph-vis';
+
+import './styles.scss';
 
 export default function AF() {
+  const nodes = [
+    { id: 1, label: "q0", color: "#345C98" },
+    { id: 2, label: "q1", color: "#345C98" },
+    { id: 3, label: "q2", color: "#345C98" },
+    { id: 4, label: "q3", color: "#345C98" }
+  ];
+
+  const edges = [
+    { from: 1, to: 2 },
+    { from: 2, to: 3 },
+    { from: 3, to: 3 },
+    { from: 3, to: 4 },
+    { from: 4, to: 3 },
+    { from: 4, to: 4 }
+  ];
+
+  const options = {
+    layout: {
+      hierarchical: false
+    },
+    edges: {
+      color: "#000000"
+    },
+    nodes: {
+      color: "#FFFFFF"
+    },
+    physics: {
+      enabled: false
+    }
+  };
+
+  const network = {
+    counter: 5,
+    graph: {
+      nodes,
+      edges
+    },
+    events: {
+      select: ({ nodes, edges }) => {
+        console.log("Selected nodes:");
+        console.log(nodes);
+        console.log("Selected edges:");
+        console.log(edges);
+        alert("Selected node: " + nodes);
+      }
+    }
+  };
+
+  const { graph, events } = network;
+
   return (
-    <div className='page-container'>
-      <h1>Autômato Finito</h1>
+    <div className='af-container'>
+      <Graph graph={graph} options={options} events={events} style={{ height: "640px" }} />
     </div>
   );
 }
